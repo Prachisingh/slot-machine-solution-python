2ffa089abdad020dd473205f759f560277f07150
Prachi Singh <prachi.singh@hungrybeargaming.co.uk>
2024-09-16 15:52:09 +0530

    Code refractor

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from WinData import WinData
 import random as rd
 
 stop_position_list = []
-slot_face =[]
+slot_face = []
 
 reels = GameConfiguration.get_reels()
 
@@ -22,7 +22,7 @@ for i in reels:
     slot_face.insert(i, slotFaceReel)
 
 
-def check_for_win_combination(sym_to_compare, slot_face):
+def check_for_win_combination(sym_to_compare, board):
     win_data = WinData()
     pos_list = []
     current_col = 0
@@ -33,7 +33,7 @@ def check_for_win_combination(sym_to_compare, slot_face):
         if col - current_col > 1:
             break
         for row in range(0, GameConfiguration.BoardHeight):
-            current_sym = slot_face[col][row]
+            current_sym = board[col][row]
             if sym_to_compare == current_sym:
                 sym_count_per_column = sym_count_per_column +1
 
@@ -55,8 +55,6 @@ def check_for_win_combination(sym_to_compare, slot_face):
 
 def populate_win(win_data, stake):
     pay_out = GameConfiguration.get_pay_out()[win_data.symbol_name]
-
-    symbol_win = 0
     ways = 1
     if pay_out is not None and win_data.sym_count_on_each_col.__len__() >= pay_out.minimum_match:
         symbol_win = pay_out.get_win_amount(win_data.sym_count_on_each_col.__len__())
@@ -69,11 +67,11 @@ def populate_win(win_data, stake):
     else:
         win_data.win_amount = 0
 
-def calculate_win(slot_face, stake):
+def calculate_win(board, stake):
     win_data_list = []
     total_win = 0
     for row in range(0, GameConfiguration.BoardHeight):
-        sym_to_compare = slot_face[0][row]
+        sym_to_compare = board[0][row]
         exist = False
         for sym in win_data_list: # TODO
             if sym.symbol_name == sym_to_compare:
@@ -81,7 +79,7 @@ def calculate_win(slot_face, stake):
 
         if win_data_list.__len__() != 0 and exist:
             continue
-        win_data = check_for_win_combination(sym_to_compare, slot_face)
+        win_data = check_for_win_combination(sym_to_compare, board)
         populate_win(win_data, stake)
         if win_data.win_amount != 0:
             total_win += win_data.win_amount
